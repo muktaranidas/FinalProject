5d01036dfc16889aa829bfb028af289c7ea1e255
Mukta-Rani-Das <mukta35-2733@diu.edu.bd>
2024-04-15 01:26:30 -0700

    done navbar

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,8 +1,8 @@
 // import React from 'react';
 const Home = () => {
     return (
-        <div>
-            {/* <h1>This is Home</h1> */}
+        <div className="mx-5">
+            <h1>This is Home</h1>
         </div>
     );
 };
--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Navbar = () => {
+  const menuItems = <React.Fragment>
+        <li><Link to='/home'>Home</Link></li>
+        <li><Link to='/about'>About</Link></li>
+        <li><Link to='/appointment'>Appointment</Link></li>
+        <li><Link to='/reviews'>Reviews</Link></li>
+        <li><Link to='/contact us'>Contact Us</Link></li>
+        <li><Link to='/login'>Login</Link></li>
+  </React.Fragment>
     return (
         <div className="navbar bg-base-100">
   <div className="navbar-start">
@@ -9,32 +17,14 @@ const Navbar = () => {
         <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-        <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
-        <li><a>Item 3</a></li>
+        {menuItems}
       </ul>
     </div>
-    <a className="btn btn-ghost text-xl">daisyUI</a>
+    <Link to='/home' className="btn btn-ghost text-xl">Doctors Portal</Link>
   </div>
   <div className="navbar-center hidden lg:flex">
     <ul className="menu menu-horizontal px-1">
-      <li><a>Item 1</a></li>
-      <li>
-        <details>
-          <summary>Parent</summary>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </details>
-      </li>
-      <li><a>Item 3</a></li>
+      {menuItems}
     </ul>
   </div>
   <div className="navbar-end">
